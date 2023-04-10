d552d8533fa20777a5c21d94f110876d24389999
Ben Border <benborder@users.noreply.github.com>
2023-04-10 16:32:55 +1000

    Remove unnecessary cudnn binaries included from libtorch as they are already included in base cuda image

--- dl-libtorch.bash
+++ dl-libtorch.bash
@@ -8,3 +8,8 @@ wget https://download.pytorch.org/libtorch/${compute_type}/libtorch-cxx11-abi-sh
 
 # Extract to /tmp and cleanup zip
 unzip /tmp/libtorch-cxx11-abi-shared-with-deps-${version}+${compute_type}.zip -d /tmp/
+
+# If using cuda, cudnn is already included in base image, so remove it from libtorch directory
+if [[ "${compute_type:0:2}" == "cu" ]]; then
+    rm /tmp/libtorch/lib/libcudnn*
+fi
