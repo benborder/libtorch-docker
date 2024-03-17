4ca930f8d37d28de7e71b442bb6364829595bd63
Ben Border <benborder@users.noreply.github.com>
2024-03-17 11:41:16 +1000

    Install protobuf version 3.13 from source to match libtorch's version

--- dev/setup-dev.bash
+++ dev/setup-dev.bash
@@ -16,6 +16,14 @@ apt-get update && ACCEPT_EULA=Y apt-get install --assume-yes --no-install-recomm
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
+# Install protobuf from source as 3.12 is provided in ubuntu 22.04 but libtorch currently requires 3.13
+git clone --depth 1 --branch v3.13.0.1 https://github.com/protocolbuffers/protobuf.git \
+	&& cd protobuf/cmake \
+	&& cmake -Dprotobuf_BUILD_TESTS=OFF -B build \
+	&& cmake --build build --parallel \
+	&& cmake --install build \
+	&& cd ../.. && rm -rf protobuf
+
 # Install gperf
 git clone --depth 1 --branch gperftools-2.15 https://github.com/gperftools/gperftools \
 	&& cd gperftools \
